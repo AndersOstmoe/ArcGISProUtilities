ed8d4712cdb332bc7d38b13cc9f7b2c5c03776cd
AndersOstmoe <46564396+AndersOstmoe@users.noreply.github.com>
2019-04-25 15:19:26 +0200

    Sjekk inn 2

--- FindMaxDate.py
+++ FindMaxDate.py
@@ -1,24 +1,24 @@
 # -*- coding: UTF-8 -*-
-import arcpy, datetime
+import arcpy
+import datetime
 
-Table = arcpy.GetParameterAsText(0)
-Field = arcpy.GetParameterAsText(1)
 
 def find_newest_date(table, field):
-    newestdate = datetime.datetime(2017, 1, 1)
+    newest_date = datetime.datetime(2017, 1, 1)
     with arcpy.da.SearchCursor(table, field) as cursor:
         for row in cursor:
             date = row[0]
-            if (date > newestdate):
-                newestdate = date
+            if date > newest_date:
+                newest_date = date
             print(date)
 
-    print (" ")
-    print(newestdate)
-    return newestdate
+    return newest_date
 
 
-Newest_date = find_newest_date(Table, Field)
+InputTable = arcpy.GetParameterAsText(0)
+InputField = arcpy.GetParameterAsText(1)
+
+Newest_date = find_newest_date(InputTable, InputField)
 
 arcpy.SetParameterAsText(2, Newest_date)
 
--- test_functions.py
+++ test_functions.py
@@ -1,7 +1,11 @@
 import FindMaxDate
 
-def test_find_max_date():
-    FindMaxDate.find_newest_date("C:\DiskTemp\Innspill\Innspill.gdb\Innspill", "CreationDate")
 
-test_find_max_date()
+def test_find_max_date():
+    table = r"C:\DiskTemp\Innspill\Innspill.gdb\Innspill"
+    field = "CreationDate"
+    return FindMaxDate.find_newest_date(table, field)
+
+
+print(test_find_max_date())
 
